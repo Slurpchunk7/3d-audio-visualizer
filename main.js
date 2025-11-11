e6fb170fcc61275e694ae6e737473b9d362ebf6e
Slurpchunk7 <slurpchunk7@gmail.com>
2025-11-11 19:48:38 +1100

    Made three import from url

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.166.0/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 let scene, camera, renderer;
